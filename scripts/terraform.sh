98d1f6836b3503efdc25562ba1649ce6eae3f901
r_y <risktakerxxx@gmail.com>
2021-07-29 22:13:09 +0900

    Update terraform.sh

--- scripts/terraform.sh
+++ scripts/terraform.sh
@@ -8,10 +8,13 @@
 # @parameter $2 環境名 dev|fut
 # @return 1 エラー
 #==================================================================
+terraform_cmd=$1
+env=$2
+
 
 # tfenvインストール
 git clone https://github.com/tfutils/tfenv.git .tfenv
-export PATH=/home/circleci/project/.tfenv/bin:$PATH
+export PATH=/home/circleci/project/.tfenv/bin:${PATH}
 tfenv -v
 tfenv list-remote
 tfenv install 0.11.8
@@ -22,4 +25,12 @@ git clone git@github.com:r-ymgc/terraform.git terraform
 
 # Terraform実行
 cd terraform/web_deploy
-terraform $1 -var-file=../$2.tfvars
+
+if [[ "${terraform_cmd}" == "apply" ]]; then
+  terraform plan -var-file=../${env}.tfvars -out=./app.plan
+  terraform apply "./app.plan"
+elif [[ "${terraform_cmd}" == "init" ]]; then
+  terraform init -verify-plugins=false
+else
+  terraform plan -var-file=../${env}.tfvars
+fi
